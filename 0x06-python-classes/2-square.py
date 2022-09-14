a837e1f0e51bc2ec5dd21482bd38d2e8e5bd15cc
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-09-14 00:50:06 -0700

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""Define a class Square."""
-
-
 class Square:
-    """Represents a square.
-
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError('size must be an integer')
