8efed91b0d7fdf3c99ef9f918bd83be161c019bf
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2022-09-14 04:38:31 -0700

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
 
 class Square:
-    """Represents a square.
+    """Represent a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
-        """ 
-        self.__size = size
+        """
+        self.size = size
 
     @property
     def size(self):
         """Get/set the current size of the square."""
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -27,8 +28,8 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Returns the current square area."""
-        return self.__size ** 2
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
     def my_print(self):
         """Print the square with the # character."""
